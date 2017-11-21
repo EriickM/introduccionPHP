83d376fcd7a5b5bbad3d997d1c670ab27faac791
EriickM <eriickdrag@gmail.com>
2017-11-20 20:44:01 -0600

    Borrado de datos conexion password

--- models/conexion.php
+++ models/conexion.php
@@ -7,7 +7,7 @@
 		
 		public function conectar()
 		{
-			$link = new PDO("mysql:host=localhost;dbname=cursophp","root","TlatepA12");
+			$link = new PDO("mysql:host=localhost;dbname=cursophp","root","");
 			return $link;
 		}
 	}
